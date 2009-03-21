8c776a24110e7db19baacabc56978fed6b3b6d57
Martin Kleppmann <martin@eptcomputing.com>
2009-03-21 14:35:28 +0000

    First experiments with scala's XML parsing

--- src/org/oaccounts/Main.scala
+++ src/org/oaccounts/Main.scala
@@ -1,7 +1,39 @@
 package org.oaccounts
 
+import scala.xml.{Elem, XML, PrettyPrinter, Utility}
+
 object Main {
+  
   def main(args: Array[String]) { 
-    println("Hello, world!") 
+    val xml = XML.loadFile("example/customers/lovely_customer_inc/1234.xml")
+    
+    // println(new PrettyPrinter(80, 4).format(xml))
+    
+    /*
+    val result = xml match {
+      case <ubl:Invoice>{ ch @ _* }</ubl:Invoice> => println(new PrettyPrinter(80, 4).format(ch))
+    }
+    */
+
+    var tax_total = 0.0
+    var net_total = 0.0
+    
+    for (line <- xml \\ "InvoiceLine") {
+      for (tax_amount <- line \ "TaxTotal" \ "TaxAmount") {
+        tax_total += tax_amount.text.toDouble
+      }
+      for (net_amount <- line \ "LineExtensionAmount") {
+        net_total += net_amount.text.toDouble
+      }
+    }
+    
+    println("Tax total: " + tax_total)
+    println("Net total: " + net_total)
+    
+    val result = xml match {
+      case Elem(pref, tag, attr, scope, ch @ _*) => Elem(pref, "badger", attr, scope, ch : _*)
+      // case <Invoice>{ ch @ _* }</Invoice> => <Invoice>{ ch }</Invoice>
+    }
+    println(new PrettyPrinter(80, 4).format(result))
   }
 }
