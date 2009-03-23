6692d9b24b7af35112f1ce64c16d032634d9c7a3
martin <martin@36f7a22c-cd02-4aa1-815a-27165e137ae5>
2009-03-23 23:21:00 +0000

    DocGen now outputs attributes; lots of tweaks
    git-svn-id: https://intra.eptcom.co.uk/svn/ept/ubl/trunk/doc@2491 36f7a22c-cd02-4aa1-815a-27165e137ae5

--- javascripts/xmldoc.js
+++ javascripts/xmldoc.js
@@ -1,14 +1,31 @@
-function hijackLinks(query) {
+function unfoldChildren(query) {
 	query.click(function(evt) {
 		evt.preventDefault();
 		var frag = this.href.replace(/(.*)(\.html)$/, '$1_frag$2');
 		$(this).parent().load(frag, function() {
-			hijackLinks($(this).find('a'));
+			unfoldChildren($(this).find('a.open'));
+			unfoldAttributes($(this).find('div.tag'));
 		});
 		return false;
 	});
 }
 
+function unfoldAttributes(query) {
+	query.each(function() {
+		var attrs = $(this).find('span.att.optional');
+		if (attrs.length > 0) {
+			$(attrs.get(attrs.length - 1)).after('<a href="#" class="showatt">...</a>');
+			$(this).find('a.showatt').click(function(evt) {
+				evt.preventDefault();
+				$(this).parent().find('span.att.optional').css('display', 'inline');
+				$(this).css('display', 'none');
+				return false;
+			});
+		}
+	});
+}
+
 $(document).ready(function() {
-	hijackLinks($('div.el a'));
+	unfoldChildren($('div.el a.open'));
+	unfoldAttributes($('div.tag'));
 });
